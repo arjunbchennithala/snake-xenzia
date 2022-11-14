26e86b943f810ca4c1c40a1afb661692272ab37e
XD <rockistangos@gmail.com>
2022-11-14 19:25:07 +0530

    Not good patch.js
    Ith nee onn nokk

--- game.js
+++ game.js
@@ -1,6 +1,9 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext('2d');
 var scorewindow = document.getElementById("score");
+var displayx = document.getElementById("dispx");
+var displayy = document.getElementById("dispy");
+var game = document.getElementById("over");
 const scale = 10;
 const width = canvas.width;
 const height = canvas.height;
@@ -42,14 +45,17 @@ class Snake {
 	}
 
 	draw() {
-		
-		ctx.fillStyle = "yellow";
+		ctx.fillStyle = "green";
 		ctx.beginPath();
 		for(var i=0; i<=this.head; i++) {
 			ctx.rect(this.body[i].x*scale, this.body[i].y*scale, scale, scale);
 			if(i<this.head) {
 				this.body[i].x = this.body[i+1].x;
 				this.body[i].y = this.body[i+1].y;
+                                if(((this.body[i].x-1) == this.body[this.head].x) && ((this.body[i].y-1) == this.body[this.head].y))
+                        {this.gameover();
+                         
+                         }
 			} 
 		}
 		ctx.fill();
@@ -58,8 +64,17 @@ class Snake {
 	grow() {
 		this.body.push(new V2(this.body[this.head].x + this.xspeed , this.body[this.head].y + this.yspeed));
 		this.head++;
-		scorewindow.innerHTML = this.head-3;
+		scorewindow.innerHTML = (this.head-3)*10;
 	}
+        display()
+               {
+                displayx.innerHTML = this.body[this.head].x;
+                displayy.innerHTML = this.body[this.head].y;
+        }
+        gameover() {
+                    clearInterval(Interval);
+                    game.innerHTML ="GAME OVER";
+        }
 }
 
 class Food {
@@ -70,7 +85,7 @@ class Food {
 	draw() {
 		ctx.beginPath();
 		ctx.rect(this.x * scale, this.y * scale, scale, scale);
-		ctx.fillStyle = "red";
+		ctx.fillStyle = "blue";
 		ctx.fill();
 	}	
 }
@@ -88,31 +103,43 @@ document.addEventListener("keydown", (event) => {
 	switch(event.key) {
 		case 'a':
 		case 'A':
+                        if((snake.xspeed == 1)&&(snake.yspeed == 0))
+                        {
+                         break;}
 			snake.yspeed = 0;
 			snake.xspeed = -1;
 			break;
 		case 'd':
 		case 'D':
+                        if((snake.xspeed == -1)&&(snake.yspeed == 0))
+                        {
+                         break;}
 			snake.yspeed = 0;
 			snake.xspeed = 1;
 			break;
 		case 'w':
 		case 'W':
+                        if((snake.xspeed == 0)&&(snake.yspeed == 1))
+                        {
+                         break;}
 			snake.xspeed = 0;
 			snake.yspeed = -1;
 			break;
 		case 's':
 		case 'S':
+                        if((snake.xpeed == 0)&&(snake.yspeed == -1))
+                        {
+                         break;}
 			snake.xspeed = 0;
 			snake.yspeed = 1;
 			break;
 	}
 });
 
-
-setInterval(()=>{
+Interval=setInterval(()=>{
 	ctx.clearRect(0, 0, width, height);
 	snake.update();
 	snake.draw();
 	food.draw();
-}, 150);
+        snake.display();
+}, 120);
